f17ecb8cb94862d1344da8862f3c2359e5d201d6
Joe Decker <blackeclipsevii@users.noreply.github.com>
2016-09-30 23:09:36 -0400

    LLW 0.04
    Things work!

--- inc/LlwLepton.h
+++ inc/LlwLepton.h
@@ -2,30 +2,19 @@
 #ifndef _LeptonWrapper_h_
 #define _LeptonWrapper_h_
 
-#ifndef _VECTOR_
-#include <vector>
-#endif
-
 #ifndef _STRING_
 #include <string>
 #endif
 
 namespace llw
 {
-    // ease of use vector for strings
-    typedef std::vector < std::string > StringVector;
-
     class Lepton {
     public:
-        // Function for interacting with lepton
-        // This function is essentially directly calling
-        //
-        // \param[in] argv a string vector of arguments
-        // \return `true` on success, `false` otherwise
-        static bool execute(StringVector &args);
+        static bool run( int argc, char** argv);
+	static void ini(size_t memSz = 0, size_t threadMemSz = 0);
     private:
         Lepton(){}
-    };
+    }; // Lepton
 } // llw
 
 #endif //_LeptonWrapper_h_
